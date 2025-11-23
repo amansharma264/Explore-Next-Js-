d2cf332e24cdfb8369af0dcc712465ef87201be7
amansharma264 <amankumar170789@gmail.com>
2025-11-23 19:15:57 +0530

    feat: add Spotlight component and enhance HeroSection with animations and improved layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { main } from "motion/react-client";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialised bg-grid-white/[0.0]">
-      <h1 className="text-2xl text-center">Chai aur Code</h1>
+      
       <HeroSection />
     </main>
   );
