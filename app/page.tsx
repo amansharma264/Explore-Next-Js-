7a2f93774f6106c1a994c57e08b2ec37d51e9678
amansharma264 <amankumar170789@gmail.com>
2025-11-25 22:53:56 +0530

    debugging

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
-import FeaturedCurses from "@/src/components/FeaturedCurses";
-import Instructors from "@/src/components/Instructors";
-import MusicSchoolTestimonials from "@/src/components/TestimonialCards";
-import HeroSection from "@/src/components/ui/HeroSection";
-import UpcomingWebinar from "@/src/components/UpcomingWebinar";
-import WhyChooseUs from "@/src/components/WhyChooseUs";
-import { main } from "motion/react-client";
+
+import FeaturedCourses from "@/components/FeaturedCurses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialised bg-grid-white/[0.0]">
-      
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
-      <FeaturedCurses />
+      <FeaturedCourses />
       <WhyChooseUs />
       <MusicSchoolTestimonials />
-      <UpcomingWebinar />
-      <Instructors /
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
     </main>
   );
 }
